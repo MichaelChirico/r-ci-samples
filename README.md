13b3a64da829dede92b60550faa522dca3b97483
Michael Chirico <michael.chirico@grabtaxi.com>
2019-12-15 13:26:38 +0800

    added comments, cleaned up a bit

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-# r-travis-samples
+# r-ci-samples
 
-Sample .travis.yml files from popular R package repos
+Sample Continuous Integration (CI) scripts from popular R package repos
 
-If you're looking to build Travis CI integration for your R project, look no further!
+The following package CI tools are tracked:
+
+ - [Travis CI](https://travis-ci.org/) : `travis`
+ - [Appveyor](https://www.appveyor.com/) : `appveyor`
+ - [Jenkins](https://jenkins.io/) : `jenkins`
+ - [Gitlab CI](https://docs.gitlab.com/ee/ci/) : `gitlab-ci`
+ 
+Also tracked are some common package metadata files:
+
+ - [.gitignore](https://git-scm.com/docs/gitignore) : `gitignore`
+ - [GNU Make/`Makefile`](https://www.gnu.org/software/make/) : `make`
+ - [`_pkgdown.yml` for `pkgdown` sites](https://pkgdown.r-lib.org/index.html) : `pkgdown`
+ - [`.Rbuildignore`](http://r-pkgs.had.co.nz/package.html) : `rbuildignore`
+ - [`.Rproj` RStudio project file](https://support.rstudio.com/hc/en-us/articles/200526207-Using-Projects) : `rproj`
+
+If you're looking to build CI integration for your R project, look no further!
 
 Build on the backs of R giants & learn from their own existing CI pipelines.
+
+The repos used here are found in the `.tracked_repos` file; please file a PR to add any new package.
+
+If there's some CI tool/metadata I'm missing, this can easily be added to the `update.R` script.
--- update.R
+++ update.R
@@ -1,5 +1,6 @@
 repos = readLines('.tracked_repos')
 
+# file patterns; e.g. appveyor can be .appveyor.yml or appveyor.yml
 known_meta = c(
   travis = '.travis.yml',
   gitignore = '.gitignore',
@@ -16,8 +17,30 @@ known_meta = c(
 sapply(names(known_meta), dir.create, showWarnings = FALSE)
 
 GH_STEM = 'https://github.com'
+# approach: clone the most recent commit of the repo, then
+#   examine the directory structure for each target meta file.
+#   Main drawback I see so far is in the file[1L] part -- it would be
+#   ideal to get the _right_ file rather than just the first one,
+#   but I haven't seen a way to do this scalably yet.
+# alternative approaches skipped [both are in commit history]:
+#   - Simply record manually which files are in which repo; this is what I did
+#     for the initial pass of including travis-ci files, but doing so for
+#     a full range of metadata files was too tedious. Besides the sunk time,
+#     (1) it becomes a perpetual burden to track when files add/remove CI
+#     options and (2) the cut-and-paste approach is suited to package-only
+#     repos; e.g., wesm/feather is a higher-level repo of which the R package
+#     is only one component, meaning the paths for R-specific files will be
+#     different than the paths for meta files, causing havoc for this approach
+#   - Use combination of svn ls (to show the file structure of each repo) and
+#     curl to pull only specific files. In principle this approach should be
+#     much preferred and much faster since I only download a very small number
+#     of files relatively, but in practice it was excruciatingly slow. I think
+#     there are some throttles set up on these gateways to GitHub that don't
+#     appear to affect the git clone API. Also, noteable that the svn ls
+#     command appears to be quite fragile for this purpose, failing often
 for (repo in repos) {
   cat('Cloning ', repo, '...\n', sep = '')
+  # clone to tmp then delete it afterwards
   git_cmd = sprintf('git clone %s/%s.git --depth 1 tmp --quiet', GH_STEM, repo)
   system(git_cmd)
   files = list.files('tmp', recursive = TRUE, all.files = TRUE)
